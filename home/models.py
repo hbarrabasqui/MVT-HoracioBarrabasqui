52cd829cde9d77d97fac20c212ccbf40bffefe90
Horacio <hbarrabasqui@gmail.com>
2022-10-08 17:33:54 -0300

    Fin 1° entrega Django

---
+++ home/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Familiar(models.Model):    
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    edad = models.IntegerField()
+    fecha_creacion = models.DateField(null=True)
+    
+
+
+
+
+
